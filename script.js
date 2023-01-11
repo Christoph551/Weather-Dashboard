45f4ae78fdd69dfeeb0b115917570e9b3807ba4e
Christoph551 <christophersimmonds551@gmail.com>
2023-01-10 17:12:56 -0700

    Created preliminary layout with HTML and CSS.

---
+++ script.js
@@ -0,0 +1,4 @@
+// Need event listener on submit button
+// Need to maintain local storage of user's input for city name
+// Need to link a viable weather API to reference.
+// Also need to import day.js to display current date
