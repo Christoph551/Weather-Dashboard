4fa4da57a9d58fd94065386785afedf7f125ab43
Christoph551 <christophersimmonds551@gmail.com>
2023-01-11 20:26:53 -0700

    Saving changes. Still haven't successfully implemented API.

--- script.js
+++ script.js
@@ -1,46 +1,65 @@
-// Need event listener on submit button. May also add loading animation if requestUrl takes a bit.
 // Need to maintain local storage of user's input for city name
-// Need to link a viable weather API to reference.
-// Also need to import day.js to display current date
 
-const apiKey = '63b77ed365bd0d35dba55f456d174d34'; // https://home.openweathermap.org/api_keys
 
 const $main = document.querySelector('#main');
-const dateFormat = dayjs().format("MMMM D, YYYY");
-const $currentDate = document.querySelector('#currentDate');
 const $submitBtn = document.querySelector('#submitBtn');
 const $userInput = document.querySelector('#userInput');
 
-// Need to figure out a for loop to iterate through API data and then append to newly
-// created elements.
-// for (let i = 0; i < data.length) 
+let submitEventHandler = function (event) {
+    event.preventDefault();
+    
+    let input = $userInput.value.trim();
+    
+    if (!input) {
+        return;
+    }
+    
+    getApi(requestUrl);
+    
+}
+// created elements for date display
+const dateFormat = dayjs().format("MMMM D, YYYY");
+const $currentDate = document.querySelector('#currentDate');
 const $figure = document.createElement('figure');
 const $figContent = document.createElement('div');
 const $currentDay = document.createElement('h2');
-    $figure.setAttribute("class","figure");
-    $figContent.setAttribute("class","weatherDisplay");
-    $currentDay.innerHTML = dateFormat;       
-    $currentDay.setAttribute("class","todaysDate");
-    $currentDate.append(dateFormat);
 
-function submitEventHandler (event) {
-    event.preventDefault();
-    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${$userInput}&appid=${apiKey}`;
-    searchedCity = $userInput.val();
+// These are a quick note for myself to iterate the necessary elements through for loop.
+// Previous Searched Cities as well as the main element all together.
+$figure.setAttribute("class","figure");
+$figContent.setAttribute("class","weatherDisplay");
+$currentDay.innerHTML = dateFormat;       
+$currentDay.setAttribute("class","todaysDate");
+$currentDate.append(dateFormat);
 
-    // if () {
 
-    // }
+
+let getApi = function (weather) {
+    // input = $userInput.val();
+    
+    fetch(requestUrl)
+    .then(function (response) {
+        const apiKey = '63b77ed365bd0d35dba55f456d174d34'; // https://home.openweathermap.org/api_keys
+        const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${$userInput}&appid=${apiKey}`;
+        
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayWeather(data.items, weather);
+            });
+        } else {
+            alert('Error: We could not find the requested information')
+        }
+    });
 }
+console.log(requestUrl);
 
 
 
 
 
 
-
-
-
+// Need to figure out a for loop to iterate through API data and then append to newly created elements.
+// for (let i = 0; i < weather.length)
 
 
 
